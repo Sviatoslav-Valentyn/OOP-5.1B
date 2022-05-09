62bfd571575c647c189cdac5f10e2acacae656a1
Sviatoslav-Valentyn <Sviatoslav-Valentyn.Hvozdyk.IT.2021@lpnu.ua>
2022-05-09 21:55:17 +0300

    first

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../OOP 5.1B/Point.h"
+#include "../OOP 5.1B/Point.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			Point x(1, 1);
+			double test = x.Angle();
+			Assert::AreEqual(45., test);
+		}
+	};
+}
